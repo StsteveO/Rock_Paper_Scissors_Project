7789ba71267e11ec02130ff8d2d571dbcb7d6bfa
Steve Omari <steveomari62@gmail.com>
2022-09-14 08:23:20 -0400

    create output from users choice

--- javascript.js
+++ javascript.js
@@ -3,31 +3,30 @@ console.log ("Hello world!")
 // ask the user to pick rock, paper, or scissors
         // usersRawChoice
 let usersRawChoice= prompt("Pick one?", "Rock, Paper, or Scissors");
-//////////////////////works, asks user for choice
+//!!!!!!!!!!works, asks user for choice
 // have the users choice translate to one of the three options despite how they typed it
         // usersChoiceInterpretedKey
-let usersChoiceInterpretedKey= usersRawChoice.toLowerCase();
-//////////////////////works, translates choice to all lower case
+//XXXXXXXXXX let usersChoiceInterpretedKey= usersRawChoice.toLowerCase();
+//!!!!!!!!!!works, translates choice to all lower case
+//!!!!!!!!!!needed to remove due to error trying .toLowerCase null, unable */
 // display users choice (final)
 // and create function so user does not type something random
         // usersChoiceInterpreted
 function usersChoiceInterpreted() {
-        if (usersChoiceInterpretedKey==="rock") {
+        if (usersRawChoice==="" || usersRawChoice=== null) {
+                return ("turn skipped");
+        } else if (usersRawChoice.toLowerCase()==="rock") {
                 return ("rock");
-        } else if (usersChoiceInterpretedKey==="paper") {
+        } else if (usersRawChoice.toLowerCase()==="paper") {
                 return ("paper");  
-        } else if (usersChoiceInterpretedKey==="scissors") {
+        } else if (usersRawChoice.toLowerCase()==="scissors" || usersRawChoice.toLowerCase()==="scissor") {
                 return ("scissors");
-        } else if (usersChoiceInterpretedKey==="") {
-                return ("turn skipped");
-        } else if (usersRawChoice=== null) {
-                return ("turn skipped");
         } else {
                 return ("not valid response");
         }
 }
-//////////////////////somewhat works, to to fix null so when user cancelled it reads "turn skipped"
-
+//!!!!!!!!!!somewhat works, to to fix null so when user cancelled it reads "turn skipped"
+//!!!!!!!!!! works now due to removing usersChoiceInterpretedKey, and putting .toLowerCase into the function
 console.log (   usersChoiceInterpreted()   );
 
 // ask computer to pick rock, paper, or scissors
